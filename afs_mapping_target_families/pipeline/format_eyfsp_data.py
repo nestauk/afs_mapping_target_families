172c7e95cdcea281b096e5497cca3dfe4e339cdd
Emily Bicks <emilybicks@NESTA-CQ6PWN4772.home>
2023-01-30 11:27:52 +0000

    adding la level eyfsp data

---
+++ afs_mapping_target_families/pipeline/format_eyfsp_data.py
@@ -0,0 +1,9 @@
+from nesta_ds_utils.loading_saving.S3 import upload_obj
+from afs_mapping_target_families import BUCKET_NAME
+from afs_mapping_target_families.getters.raw.eyfsp import get_la_eyfsp
+
+if __name__ == "__main__":
+    eyfsp_data = get_la_eyfsp()
+    eyfsp_data = eyfsp_data[eyfsp_data["geographic_level"] == "Local authority"]
+
+    upload_obj(eyfsp_data, BUCKET_NAME, "processed/eyfsp_la_level.csv")
